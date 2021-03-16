220b0d2b74ff05ec7a9eb928689f5585f5a1166c
Ashish0070 <f20180239@pilani.bits-pilani.ac.in>
2021-03-16 23:33:44 +0530

    merged cat and dog

--- animal.py
+++ animal.py
@@ -3,21 +3,19 @@ import sys
 def default():
     print('Hello')
 
-<<<<<<< HEAD
 def cat():
     print('Meow')
 
-def main():
-    if sys.argv[1] == 'cat':
-        cat()
-=======
 def dog():
     print('woof')
 
 def main():
-    if sys.argv[1] == 'dog':
+    if sys.argv[1] == 'cat':
+        cat()
+
+    elif sys.argv[1] == 'dog':
         dog()
->>>>>>> dog
+
     else:
         default()
 
